948a2fc141a20cab93509bf2ff6e7dcfcd334fdf
Eugen Dirksen <eugen@candlesofwisdom.com>
2022-06-25 14:57:44 +0200

    Created iframe

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+import styled from 'styled-components';
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <IframeWrapper>
+      <StyledIframe src="https://capstone-project-78it4ns3z-oy-gen.vercel.app/"/>
+      </IframeWrapper>
   );
 }
+
+const StyledIframe = styled.iframe`
+border-style: none;
+  width: 375px;
+  height: 667px;
+`;
+
+const IframeWrapper =styled.body `
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: black;
+`
